d452ffb2905925666305cc8f253e9e49225741d7
Kavya K R <radhakavya68@gmail.com>
2026-09-19 17:19:24 +0530

    Create 3483-Unique3DigitEvenNumbers

---
+++ 3483-Unique3DigitEvenNumbers/3483-Unique3DigitEvenNumbers.java
@@ -0,0 +1,28 @@
+// Last updated: 9/19/2026, 5:19:26 PM
+class Solution {
+    public int totalNumbers(int[] digits) {
+        boolean[] used = new boolean[1000];
+        int count = 0;
+        for (int i = 0; i < digits.length; i++) {
+            for (int j = 0; j < digits.length; j++) {
+                for (int k = 0; k < digits.length; k++) {
+                    if (i == j || i == k || j == k) {
+                        continue;
+                    }
+                    if (digits[i] == 0) {
+                        continue;
+                    }
+                    if (digits[k] % 2 != 0) {
+                        continue;
+                    }
+                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                    if (!used[num]) {
+                        used[num] = true;
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
