37ce5fade414a096d39d21e64a2df02a2c50fcaf
Kavya K R <radhakavya68@gmail.com>
2026-09-19 17:19:16 +0530

    Create 3731-FindMissingElements

---
+++ 3731-FindMissingElements/3731-FindMissingElements.java
@@ -0,0 +1,23 @@
+// Last updated: 9/19/2026, 5:19:17 PM
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+       Arrays.sort(nums);
+        List<Integer>ans=new ArrayList<>();
+        int end=nums[nums.length-1],index=0,cur=nums[0];
+        while(cur<end){
+            if(nums[index]==cur){
+                cur++;
+                index++;
+                continue;
+            }
+            else{
+                while(cur<nums[index]){
+                    ans.add(cur++);
+                }
+                index++;
+                cur++;
+            }
+        }
+        return ans;
+    }
+}
